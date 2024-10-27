1b7b35148734786d3f1c4b2eceb467e2c258e103
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-10-27 14:03:37 +0100

    implement error chains

--- errors.go
+++ errors.go
@@ -6,38 +6,79 @@ import (
 	"strings"
 )
 
-// ErrRequired signals that a required field is missing.
-type ErrRequired struct{ Target string }
+// A chainContinuer allows us to continue a chain of errors.
+// Instead of "info: name is required", we can have "info.name is required".
+type chainContinuer interface{ continueChain(*strings.Builder) }
 
-// Error returns the name of the missing field.
+var (
+	_ chainContinuer = (*ErrRequired)(nil)
+	_ chainContinuer = (*ErrInvalid[string])(nil)
+	_ chainContinuer = (*ErrField)(nil)
+	_ chainContinuer = (*ErrIndex)(nil)
+	_ chainContinuer = (*ErrKey)(nil)
+)
+
+// ErrRequired signals that a required value is missing.
+type ErrRequired struct{}
+
+// Error fulfills the error interface.
+// Without an error chain, it simply says "a value is required".
 func (e *ErrRequired) Error() string {
-	return fmt.Sprintf("%s is required", e.Target)
+	b := &strings.Builder{}
+	b.WriteString("a value")
+	e.continueChain(b)
+	return b.String()
 }
 
-// ErrInvalid signals that a field has an invalid value.
-type ErrInvalid struct {
-	Target string
-	Value  string
-	// Enum    []string
+func (e *ErrRequired) continueChain(b *strings.Builder) {
+	b.WriteString(" is required")
+}
+
+// ErrInvalid signals that a value is invalid.
+type ErrInvalid[T comparable] struct {
+	// The value that is invalid.
+	Value T
+	// An optional list of valid values.
+	Enum []T
+	// An optional message that explains the error.
 	Message string
 }
 
 // Error returns helpful information about the invalid field and how to fix it.
-func (e *ErrInvalid) Error() string {
+func (e *ErrInvalid[_]) Error() string {
 	b := &strings.Builder{}
-	fmt.Fprintf(b, "%s (%q) is invalid", e.Target, e.Value)
+	b.WriteString("a value")
+	e.continueChain(b)
+	return b.String()
+}
+
+func (e *ErrInvalid[T]) continueChain(b *strings.Builder) {
+	switch val := any(e.Value).(type) {
+	case bool:
+		fmt.Fprintf(b, " (%t)", val)
+	default:
+		var zero T
+		if e.Value != zero {
+			fmt.Fprintf(b, " (%#v)", e.Value)
+		}
+	}
+
+	b.WriteString(" is invalid")
 	if e.Message != "" {
 		b.WriteString(": ")
 		b.WriteString(e.Message)
-		return b.String()
+		return
 	}
 
-	// if len(e.Enum) > 0 {
-	// 	b.WriteString(", must be one of: ")
-	// 	b.WriteString(strings.Join(e.Enum, ", "))
-	// }
+	if len(e.Enum) > 0 {
+		b.WriteString(", must be one of: ")
+		enums := make([]string, len(e.Enum))
+		for i, v := range e.Enum {
+			enums[i] = fmt.Sprintf("%#v", v)
+		}
 
-	return b.String()
+		b.WriteString(strings.Join(enums, ", "))
+	}
 }
 
 // ErrField is an error that occurred in a field.
@@ -50,28 +91,28 @@ type ErrField struct {
 func (e *ErrField) Error() string {
 	b := &strings.Builder{}
 	b.WriteString(e.Field)
+	return writeOutChain(b, e.Err)
+}
 
-	err := e.Err
+func (e *ErrField) continueChain(b *strings.Builder) {
+	b.WriteString(".")
+	b.WriteString(e.Field)
+}
+
+func writeOutChain(b *strings.Builder, err error) string {
 	for {
-		switch et := err.(type) {
-		case *ErrField:
-			b.WriteString(".")
-			b.WriteString(et.Field)
-		case *ErrIndex:
-			fmt.Fprintf(b, "[%d]", et.Index)
-		case *ErrKey:
-			fmt.Fprintf(b, "[%q]", et.Key)
-		case *ErrRequired:
-			b.WriteString(".")
-			b.WriteString(et.Target)
-			b.WriteString(" is required")
-			return b.String()
-		default:
-			fmt.Fprintf(b, ": %v", err)
-			return b.String()
+		// check if the error is a chainContinuer
+		if cont, ok := err.(chainContinuer); ok {
+			cont.continueChain(b)
+			err = errors.Unwrap(err)
+			continue
 		}
 
-		err = errors.Unwrap(err)
+		if err != nil {
+			fmt.Fprintf(b, ": %v", err)
+		}
+
+		return b.String() // end of the chain
 	}
 }
 
@@ -84,9 +125,17 @@ type ErrIndex struct {
 	Err   error
 }
 
+func (e *ErrIndex) continueChain(b *strings.Builder) {
+	fmt.Fprintf(b, "[%d]", e.Index)
+}
+
 // Error returns the index and the error message.
 // However, most of the time, it is wrapped in an ErrField and the index is shown as part of a path.
-func (e *ErrIndex) Error() string { return fmt.Sprintf("%d: %v", e.Index, e.Err) }
+func (e *ErrIndex) Error() string {
+	b := &strings.Builder{}
+	e.continueChain(b)
+	return writeOutChain(b, e.Err)
+}
 
 // Unwrap returns the wrapped error.
 func (e *ErrIndex) Unwrap() error { return e.Err }
@@ -97,9 +146,17 @@ type ErrKey struct {
 	Err error
 }
 
+func (e *ErrKey) continueChain(b *strings.Builder) {
+	fmt.Fprintf(b, "[%q]", e.Key)
+}
+
 // Error returns the key and the error message.
 // However, most of the time, it is wrapped in an ErrField and the key is shown as part of a path.
-func (e *ErrKey) Error() string { return fmt.Sprintf("%q: %v", e.Key, e.Err) }
+func (e *ErrKey) Error() string {
+	b := &strings.Builder{}
+	e.continueChain(b)
+	return writeOutChain(b, e.Err)
+}
 
 // Unwrap returns the wrapped error.
 func (e *ErrKey) Unwrap() error { return e.Err }
