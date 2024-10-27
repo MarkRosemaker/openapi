f5b5a1a859254df4a1d3abcf4f9085852940cd04
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-10-27 14:15:13 +0100

    Create media_range.go

---
+++ media_range.go
@@ -0,0 +1,16 @@
+package openapi
+
+import (
+	"mime"
+)
+
+// MediaRange represents a media type or media type range. It is the key type in the Content map.
+// See [RFC7231 Appendix D], and the value describes it. For requests that match multiple keys, only the most specific key is applicable. e.g. text/plain overrides text/*
+//
+// [RFC7231]: https://datatracker.ietf.org/doc/html/rfc7231#appendix-D
+type MediaRange string
+
+func (mr MediaRange) Validate() error {
+	_, _, err := mime.ParseMediaType(string(mr))
+	return err
+}
