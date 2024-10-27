233e21e97c13f50da02e929788236ca93a9fa60a
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-10-27 14:00:53 +0100

    implement content

---
+++ content.go
@@ -0,0 +1,40 @@
+package openapi
+
+import (
+	"iter"
+
+	_json "github.com/MarkRosemaker/openapi/internal/json"
+	"github.com/go-json-experiment/json"
+	"github.com/go-json-experiment/json/jsontext"
+)
+
+type Content map[MediaRange]*MediaType
+
+func (c Content) Validate() error {
+	for mr, mt := range c.ByIndex() {
+		if err := mr.Validate(); err != nil {
+			return &ErrKey{Key: string(mr), Err: err}
+		}
+
+		if err := mt.Validate(); err != nil {
+			return &ErrKey{Key: string(mr), Err: err}
+		}
+	}
+
+	return nil
+}
+
+// ByIndex returns the keys of the map in the order of the index.
+func (c Content) ByIndex() iter.Seq2[MediaRange, *MediaType] {
+	return _json.OrderedMapByIndex(c, getIndexMediaType)
+}
+
+// UnmarshalJSONV2 unmarshals the map from JSON and sets the index of each variable.
+func (c *Content) UnmarshalJSONV2(dec *jsontext.Decoder, opts json.Options) error {
+	return _json.UnmarshalOrderedMap(c, dec, opts, setIndexMediaType)
+}
+
+// MarshalJSONV2 marshals the map to JSON in the order of the index.
+func (c *Content) MarshalJSONV2(enc *jsontext.Encoder, opts json.Options) error {
+	return _json.MarshalOrderedMap(c, enc, opts)
+}
---
+++ content_test.go
@@ -0,0 +1,90 @@
+package openapi_test
+
+import (
+	"testing"
+
+	"github.com/MarkRosemaker/openapi"
+)
+
+func TestContent_JSON(t *testing.T) {
+	t.Parallel()
+
+	// Example
+	testJSON(t, []byte(`{
+  "application/json": {
+    "schema": {
+         "$ref": "#/components/schemas/Pet"
+    },
+    "examples": {
+      "cat" : {
+        "summary": "An example of a cat",
+        "value":
+          {
+            "name": "Fluffy",
+            "petType": "Cat",
+            "color": "White",
+            "gender": "male",
+            "breed": "Persian"
+          }
+      },
+      "dog": {
+        "summary": "An example of a dog with a cat's name",
+        "value" :  {
+          "name": "Puma",
+          "petType": "Dog",
+          "color": "Black",
+          "gender": "Female",
+          "breed": "Mixed"
+        }
+      },
+      "frog": {
+        "$ref": "#/components/examples/frog-example"
+      }
+    }
+  }
+}`), &openapi.Content{})
+
+	// NOTE: Content transferred in binary (octet-stream) MAY omit `schema`
+	// a PNG image as a binary file
+	testJSON(t, []byte(`{"image/png": {}}`), &openapi.Content{})
+	// an arbitrary binary file
+	testJSON(t, []byte(`{"application/octet-stream": {}}`), &openapi.Content{})
+
+	// Binary content transferred with base64 encoding
+	// Note that the `Content-Type` remains `image/png`, describing the semantics of the payload.  The JSON Schema `type` and `contentEncoding` fields explain that the payload is transferred as text.  The JSON Schema `contentMediaType` is technically redundant, but can be used by JSON Schema tools that may not be aware of the OpenAPI context.
+	testJSON(t, []byte(`{
+    "image/png": {
+      "schema": {
+        "type": "string",
+        "contentMediaType": "image/png",
+        "contentEncoding": "base64"
+      }
+    }
+  }`), &openapi.Content{})
+
+	// These examples apply to either input payloads of file uploads or response payloads.
+}
+
+func TestContent_Validate_Error(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		c   openapi.Content
+		err string
+	}{
+		{openapi.Content{
+			"not a real media type": &openapi.MediaType{},
+		}, `["not a real media type"]: mime: expected slash after first token`},
+		{openapi.Content{
+			"application/json": &openapi.MediaType{
+				Schema: &openapi.SchemaRef{Value: &openapi.Schema{}},
+			},
+		}, `["application/json"].schema.type is required`},
+	} {
+		t.Run(tc.err, func(t *testing.T) {
+			if err := tc.c.Validate(); err == nil || err.Error() != tc.err {
+				t.Errorf("expected %q, got %q", tc.err, err)
+			}
+		})
+	}
+}
