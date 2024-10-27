0e864cfb9ef818278a1eb4dcd1de06a641a8e356
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-10-27 14:01:23 +0100

    implement data type

---
+++ data_type_test.go
@@ -0,0 +1,25 @@
+package openapi_test
+
+import (
+	"testing"
+
+	"github.com/MarkRosemaker/openapi"
+)
+
+func TestDataType(t *testing.T) {
+	// test a valid data type
+	if err := openapi.TypeInteger.Validate(); err != nil {
+		t.Fatal(err)
+	}
+
+	// test an invalid data type
+	err := openapi.DataType("foo").Validate()
+	if err == nil {
+		t.Fatal("expected an error for an invalid data type")
+	}
+
+	err = &openapi.ErrField{Field: "type", Err: err}
+	if want := `type ("foo") is invalid, must be one of: "integer", "number", "string", "array", "boolean", "object"`; want != err.Error() {
+		t.Errorf("expected %q, got %q", want, err.Error())
+	}
+}
