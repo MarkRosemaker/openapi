ba491b3444102d3012ffb9d4a0f45b6f17c01804
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-10-27 14:17:17 +0100

    implement media type

---
+++ media_type_test.go
@@ -0,0 +1,41 @@
+package openapi_test
+
+import (
+	"testing"
+
+	"github.com/MarkRosemaker/openapi"
+	"github.com/go-json-experiment/json/jsontext"
+)
+
+func TestMediaType_Validate_Error(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		mt  openapi.MediaType
+		err string
+	}{
+		{openapi.MediaType{
+			Example:  "foo",
+			Examples: openapi.Examples{},
+		}, `example and examples are mutually exclusive`},
+		{openapi.MediaType{
+			Examples: openapi.Examples{"foo": invalidExample},
+		}, `examples["foo"]: value and externalValue are mutually exclusive`},
+		{openapi.MediaType{
+			Encoding: openapi.Encodings{
+				"foo": &openapi.Encoding{Style: "bar"},
+			},
+		}, `encoding["foo"].style ("bar") is invalid, must be one of: "matrix", "label", "form", "simple", "spaceDelimited", "pipeDelimited", "deepObject"`},
+		{openapi.MediaType{
+			Extensions: jsontext.Value(`{"foo":"bar"}`),
+		}, `foo: ` + openapi.ErrUnknownField.Error()},
+	} {
+		t.Run(tc.err, func(t *testing.T) {
+			if err := tc.mt.Validate(); err == nil {
+				t.Fatal("expected error")
+			} else if err.Error() != tc.err {
+				t.Fatalf("want: %v, got: %v", tc.err, err)
+			}
+		})
+	}
+}
