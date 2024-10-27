864aa4085380fcca70ff2ba1c56893ac9030c57c
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-10-27 20:41:07 +0100

    implement request body

---
+++ request_body.go
@@ -0,0 +1,39 @@
+package openapi
+
+import "strings"
+
+// RequestBody describes a single request body.
+//
+// ([Specification])
+//
+// [Specification]: https://spec.openapis.org/oas/v3.1.0#request-body-object
+type RequestBody struct {
+	// A brief description of the request body. This could contain examples of use.
+	// CommonMark syntax MAY be used for rich text representation.
+	Description string `json:"description,omitempty" yaml:"description,omitempty"`
+	// Determines if the request body is required in the request. Defaults to `false`.
+	Required bool `json:"required,omitempty,omitzero" yaml:"required,omitempty"`
+	// REQUIRED. The content of the request body. The key is a media type or media type range and the value describes it. For requests that match multiple keys, only the most specific key is applicable. e.g. text/plain overrides text/*
+	Content Content `json:"content" yaml:"content"`
+
+	// This object MAY be extended with Specification Extensions.
+	Extensions Extensions `json:",inline" yaml:",inline"`
+}
+
+func (r *RequestBody) Validate() error {
+	r.Description = strings.TrimSpace(r.Description)
+
+	if len(r.Content) == 0 {
+		return &ErrField{Field: "content", Err: &ErrRequired{}}
+	}
+
+	if err := r.Content.Validate(); err != nil {
+		return &ErrField{Field: "content", Err: err}
+	}
+
+	if err := validateExtensions(r.Extensions); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ request_body_test.go
@@ -0,0 +1,187 @@
+package openapi_test
+
+import (
+	"testing"
+
+	"github.com/MarkRosemaker/openapi"
+)
+
+func TestRequestBody_JSON(t *testing.T) {
+	t.Parallel()
+
+	// A request body with a referenced model definition.
+	testJSON(t, []byte(`{
+  "description": "user to add to the system",
+  "content": {
+    "application/json": {
+      "schema": {
+        "$ref": "#/components/schemas/User"
+      },
+      "examples": {
+          "user" : {
+            "summary": "User Example",
+            "externalValue": "https://foo.bar/examples/user-example.json"
+          }
+        }
+    },
+    "application/xml": {
+      "schema": {
+        "$ref": "#/components/schemas/User"
+      },
+      "examples": {
+          "user" : {
+            "summary": "User example in XML",
+            "externalValue": "https://foo.bar/examples/user-example.xml"
+          }
+        }
+    },
+    "text/plain": {
+      "examples": {
+        "user" : {
+            "summary": "User example in Plain text",
+            "externalValue": "https://foo.bar/examples/user-example.txt"
+        }
+      }
+    },
+    "*/*": {
+      "examples": {
+        "user" : {
+            "summary": "User example in other format",
+            "externalValue": "https://foo.bar/examples/user-example.whatever"
+        }
+      }
+    }
+  }
+}`), &openapi.RequestBody{})
+
+	// A body parameter that is an array of string values:
+	testJSON(t, []byte(`{
+  "description": "user to add to the system",
+  "required": true,
+  "content": {
+    "text/plain": {
+      "schema": {
+        "type": "array",
+        "items": {
+          "type": "string"
+        }
+      }
+    }
+  }
+}`), &openapi.RequestBody{})
+
+	// A `requestBody` for submitting a file in a `POST` operation may look like the following example:
+	testJSON(t, []byte(`{
+    "content": {
+      "application/octet-stream": {}
+    }
+  }`), &openapi.RequestBody{})
+
+	// In addition, specific media types MAY be specified:
+	testJSON(t, []byte(`{
+    "content": {
+      "image/jpeg": {},
+      "image/png": {}
+    }
+  }`), &openapi.RequestBody{})
+
+	// To upload multiple files, a `multipart` media type MUST be used:
+	testJSON(t, []byte(`{
+    "content": {
+      "multipart/form-data": {
+        "schema": {
+          "type": "object",
+          "properties": {
+            "file": {
+              "type": "array",
+              "items": {}
+            }
+          }
+        }
+      }
+    }
+  }`), &openapi.RequestBody{})
+
+	// To submit content using form url encoding via RFC1866, the following definition may be used:
+	testJSON(t, []byte(`{
+    "content": {
+      "application/x-www-form-urlencoded": {
+        "schema": {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "string",
+              "format": "uuid"
+            },
+            "address": {
+              "type": "object",
+              "properties": {}
+            }
+          }
+        }
+      }
+    }
+  }`), &openapi.RequestBody{})
+
+	// `contentMediaType` and `contentEncoding`
+	testJSON(t, []byte(`{
+    "content": {
+      "multipart/form-data": {
+        "schema": {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "string",
+              "format": "uuid"
+            },
+            "address": {
+              "type": "object",
+              "properties": {}
+            },
+            "profileImage": {
+              "type": "string",
+              "contentMediaType": "image/png",
+              "contentEncoding": "base64"
+            },
+            "children": {
+              "type": "array",
+              "items": {
+                "type": "string"
+              }
+            },
+            "addresses": {
+              "type": "array",
+              "items": {
+                "$ref": "#/components/schemas/Address"
+              }
+            }
+          }
+        }
+      }
+    }
+  }`), &openapi.RequestBody{}) // TODO in the spec, it has  type: object in "items" besides the "$ref"
+}
+
+func TestRequestBody_Validate_Error(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		p   openapi.RequestBody
+		err string
+	}{
+		{openapi.RequestBody{}, "content is required"},
+		{openapi.RequestBody{
+			Content: openapi.Content{"foo; bar": &openapi.MediaType{}},
+		}, `content["foo; bar"]: mime: invalid media parameter`},
+		{openapi.RequestBody{
+			Content:    openapi.Content{"application/json": &openapi.MediaType{}},
+			Extensions: []byte(`{"foo": "bar"}`),
+		}, "foo: " + openapi.ErrUnknownField.Error()},
+	} {
+		t.Run(tc.err, func(t *testing.T) {
+			if err := tc.p.Validate(); err == nil || err.Error() != tc.err {
+				t.Errorf("expected %q, got %q", tc.err, err)
+			}
+		})
+	}
+}
