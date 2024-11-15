ef4f2436788ab7c605969c2f1a6c1f5950d7c5e5
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-11-15 16:12:17 +0100

    Update security_requirements.go

--- security_requirements.go
+++ security_requirements.go
@@ -1,8 +1,8 @@
 package openapi
 
-type Security []SecurityRequirement
+type SecurityRequirements []SecurityRequirement
 
-func (ss Security) Validate() error {
+func (ss SecurityRequirements) Validate() error {
 	for i, s := range ss {
 		if err := s.Validate(); err != nil {
 			return &ErrIndex{Index: i, Err: err}
