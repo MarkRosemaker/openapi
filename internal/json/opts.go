7a48512137875d74e8f74c5109a846d00e400ab1
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-11-20 15:42:19 +0100

    prettify JSON output

--- internal/json/opts.go
+++ internal/json/opts.go
@@ -2,6 +2,7 @@ package json
 
 import (
 	"github.com/go-json-experiment/json"
+	"github.com/go-json-experiment/json/jsontext"
 )
 
 var Options = json.JoinOptions([]json.Options{
@@ -10,4 +11,5 @@ var Options = json.JoinOptions([]json.Options{
 	json.RejectUnknownMembers(true),
 	json.WithMarshalers(URLMarshal),
 	json.WithUnmarshalers(URLUnmarshal),
+	jsontext.WithIndent("  "), // indent with two spaces
 }...)
