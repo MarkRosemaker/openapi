eff8f2ecf7d9c1ca14da7a6bf7cea7959b0c2939
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-12-20 23:26:42 +0100

    add .In method to ParameterList

--- parameter_list.go
+++ parameter_list.go
@@ -3,6 +3,8 @@ package openapi
 import (
 	"errors"
 	"fmt"
+
+	"github.com/MarkRosemaker/errpath"
 )
 
 // ParameterList is a list of parameter references.
@@ -21,11 +23,11 @@ func (p ParameterList) Validate() error {
 		// check for duplicates
 		id := parameterID{Name: param.Value.Name, Location: param.Value.In}
 
-		errNotUnique := &ErrIndex{
+		errNotUnique := &errpath.ErrIndex{
 			Index: i,
-			Err: &ErrField{
+			Err: &errpath.ErrField{
 				Field: "name",
-				Err: &ErrInvalid[string]{
+				Err: &errpath.ErrInvalid[string]{
 					Value:   param.Value.Name,
 					Message: fmt.Sprintf("not unique in %s", param.Value.In),
 				},
@@ -39,17 +41,39 @@ func (p ParameterList) Validate() error {
 		params[id] = errNotUnique
 
 		if err := param.Validate(); err != nil {
-			return &ErrIndex{Index: i, Err: err}
+			return &errpath.ErrIndex{Index: i, Err: err}
 		}
 	}
 
 	return nil
 }
 
+// In is a convenience function to filter by a specific parameter location.
+func (p ParameterList) In(in ParameterLocation) ParameterList {
+	var result ParameterList
+	for _, param := range p {
+		if param.Value.In == in {
+			result = append(result, param)
+		}
+	}
+
+	return result
+}
+
+// InPath returns all path parameters from the list.
+func (p ParameterList) InPath() ParameterList {
+	return p.In(ParameterLocationPath)
+}
+
+// InQuery returns all query parameters from the list.
+func (p ParameterList) InQuery() ParameterList {
+	return p.In(ParameterLocationQuery)
+}
+
 func (l *loader) resolveParameterList(p ParameterList) error {
 	for i, param := range p {
 		if err := l.resolveParameterRef(param); err != nil {
-			return &ErrIndex{Index: i, Err: err}
+			return &errpath.ErrIndex{Index: i, Err: err}
 		}
 	}
 
--- parameter_list_test.go
+++ parameter_list_test.go
@@ -27,3 +27,41 @@ func TestParameterList_Validate_Error(t *testing.T) {
 		t.Fatalf("want: %v, got: %v", want, err)
 	}
 }
+
+func TestParameterList_In(t *testing.T) {
+	t.Parallel()
+
+	var list openapi.ParameterList
+
+	if inPath := list.InPath(); inPath != nil {
+		t.Fatalf("want: nil, got: %v", inPath)
+	}
+
+	if inQuery := list.InQuery(); inQuery != nil {
+		t.Fatalf("want: nil, got: %v", inQuery)
+	}
+
+	list = append(list, &openapi.ParameterRef{
+		Value: &openapi.Parameter{
+			Name: "foo", In: openapi.ParameterLocationQuery,
+			Schema: &openapi.Schema{Type: openapi.TypeString},
+		},
+	}, &openapi.ParameterRef{
+		Value: &openapi.Parameter{
+			Name: "bar", In: openapi.ParameterLocationPath,
+			Schema: &openapi.Schema{Type: openapi.TypeString},
+		},
+	})
+
+	if inPath := list.InPath(); len(inPath) != 1 {
+		t.Fatalf("want: 1, got: %v", len(inPath))
+	} else if want := "bar"; inPath[0].Value.Name != want {
+		t.Fatalf("want: %q, got: %q", want, inPath[0].Value.Name)
+	}
+
+	if inQuery := list.InQuery(); len(inQuery) != 1 {
+		t.Fatalf("want: 1, got: %v", len(inQuery))
+	} else if want := "foo"; inQuery[0].Value.Name != want {
+		t.Fatalf("want: %q, got: %q", want, inQuery[0].Value.Name)
+	}
+}
