91bac371ffac64070bb3fab7edcd3b04ce8fe9c6
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-12-24 23:10:20 +0100

    marshal URLs using jsonutil

--- internal/json/opts.go
+++ internal/json/opts.go
@@ -1,6 +1,7 @@
 package json
 
 import (
+	"github.com/MarkRosemaker/jsonutil"
 	"github.com/go-json-experiment/json"
 	"github.com/go-json-experiment/json/jsontext"
 )
@@ -9,7 +10,7 @@ var Options = json.JoinOptions([]json.Options{
 	// unevaluatedProperties is set to false in most objects according to the OpenAPI specification
 	// also protect against deleting unknown fields when overwriting later
 	json.RejectUnknownMembers(true),
-	json.WithMarshalers(URLMarshal),
-	json.WithUnmarshalers(URLUnmarshal),
+	json.WithMarshalers(json.NewMarshalers(json.MarshalFuncV2(jsonutil.URLMarshal))),
+	json.WithUnmarshalers(json.NewUnmarshalers(json.UnmarshalFuncV2(jsonutil.URLUnmarshal))),
 	jsontext.WithIndent("  "), // indent with two spaces
 }...)
