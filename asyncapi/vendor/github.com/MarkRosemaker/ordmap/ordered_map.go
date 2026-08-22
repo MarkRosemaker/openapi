35836fd44aae64fcf61feb36b84c89fa74395d0a
Marco Rösler <48681726+MarkRosemaker@users.noreply.github.com>
2026-08-22 11:08:24 +0200

    Revert "feat(asyncapi): add AsyncAPI 3.1.0 parsing package"

--- asyncapi/vendor/github.com/MarkRosemaker/ordmap/ordered_map.go
+++
@@ -1,6 +0,0 @@
-package ordmap
-
-// OrderedMap is a map that can be ordered.
-type OrderedMap[K comparable, V any] map[K]Value[V]
-
-// NOTE: For the methods of OrderedMap, see the respective files.
