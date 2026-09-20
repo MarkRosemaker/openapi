869efe65a4b55f7a395f396f60d6a49993b1e3c5
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-09-20 13:29:29 +0200

    Update loader_json_test.go

--- loader_json_test.go
+++ loader_json_test.go
@@ -1,10 +1,17 @@
 package openapi
 
-import "testing"
+import (
+	"encoding/json/jsontext"
+	"errors"
+	"testing"
+)
 
 func TestLoadFromDataJSON_Error(t *testing.T) {
-	want := `unexpected end of JSON input`
-	if _, err := LoadFromDataJSON([]byte(`{`)); err == nil && err.Error() != want {
-		t.Fatalf("want: %s, got: %s", want, err)
+	if _, err := LoadFromDataJSON([]byte(`{`)); err == nil {
+		t.Fatal("expected error, got nil")
+	} else if syntErr, ok := errors.AsType[*jsontext.SyntacticError](err); !ok {
+		t.Fatalf("want: jsontext.SyntacticError, got: %T", err)
+	} else if want := `unexpected EOF`; syntErr.Err == nil || syntErr.Err.Error() != want {
+		t.Fatalf("want: %s, got: %v", want, syntErr.Err)
 	}
 }
